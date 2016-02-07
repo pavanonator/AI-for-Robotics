51f6625ae6b7a4883e459d1b41a6c1db71788ae0
pavanonator <pp428@scarletmail.rutgers.edu>
2016-02-07 16:34:29 -0500

    Update and rename particles.py to particles_unicylce.py
    still unicycle model. noise is added in sense() and move()

--- AI for Robotics/particles_unicylce.py
+++ AI for Robotics/particles_unicylce.py
@@ -1,10 +1,4 @@
-#we now have alpha# In this exercise, try to write a program that
-# will resample particles according to their weights.
-# Particles with higher weights should be sampled
-# more frequently (in proportion to their weight).
-
-# Don't modify anything below. Please scroll to the 
-# bottom to enter your code.
+# Please only modify the indicated area below!
 
 from math import *
 import random
@@ -86,10 +80,20 @@ class robot:
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
         return prob
-    
+      
     def __repr__(self):
         return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
 
+
+
+def eval(r, p):
+    sum = 0.0;
+    for i in range(len(p)): # calculate mean error
+        dx = (p[i].x - r.x + (world_size/2.0)) % world_size - (world_size/2.0)
+        dy = (p[i].y - r.y + (world_size/2.0)) % world_size - (world_size/2.0)
+        err = sqrt(dx * dx + dy * dy)
+        sum += err
+    return sum / float(len(p))
 
 #myrobot = robot()
 #myrobot.set_noise(5.0, 0.1, 5.0)
@@ -99,50 +103,43 @@ class robot:
 #myrobot = myrobot.move(-pi/2, 10.0)
 #print myrobot.sense()
 
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER/MODIFY CODE BELOW ####
 myrobot = robot()
 myrobot = myrobot.move(0.1, 5.0)
 Z = myrobot.sense()
-
 N = 1000
+T = 10 #Leave this as 10 for grading purposes.
+
 p = []
 for i in range(N):
-    x = robot()
-    x.set_noise(0.05, 0.05, 5.0)
-    p.append(x)
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
 
-p2 = []
-for i in range(N):
-    p2.append(p[i].move(0.1, 5.0))
-p = p2
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-w = []
-for i in range(N):
-    w.append(p[i].measurement_prob(Z))
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
 
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
 
-#### DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
-# You should make sure that p3 contains a list with particles
-# resampled according to their weights.
-# Also, DO NOT MODIFY p.
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+    #enter code here, make sure that you output 10 print statements.
+    print eval(myrobot, p)
 
-p3 = []
-a=[]
-sum =0
-for i in range(N):
-    sum =sum + w[i]
-    
-for i in range(N):
-    for j in range(int(w[i]/sum*N)):
-        a.append(p[i])
-
-x=len(a)
-for i in range(N):
-    p3.append(a[int(random.random()*1000%x)])
-#for i in range(N):
- #   for j in range(N):
-  #      if(w[j]>random.random()):
-   #         p3.append(p[j])
-#print len(p3)
-p=p3
-#print len(p)
-print p
